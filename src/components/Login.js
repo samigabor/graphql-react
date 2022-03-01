f714c341b97838a4fce001816d031a4ef1b7cec9
s-gabor <ing.gabor@yahoo.com>
2022-03-01 22:48:37 +0200

    add login and signup mutations, use setContext apollo middleware to attach auth token for all requests

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,23 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { gql, useMutation } from "@apollo/client";
+import { AUTH_TOKEN } from "../constants";
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
 
 const Login = () => {
   const navigate = useNavigate();
@@ -9,6 +27,30 @@ const Login = () => {
     email: "",
     password: "",
     name: "",
+  });
+
+  const [login] = useMutation(LOGIN_MUTATION, {
+    variables: {
+      email: formState.email,
+      password: formState.password,
+    },
+    onCompleted: ({ login }) => {
+      localStorage.setItem(AUTH_TOKEN, login.token);
+      navigate("/");
+      console.log("login", login);
+    },
+  });
+
+  const [signup] = useMutation(SIGNUP_MUTATION, {
+    variables: {
+      name: formState.name,
+      email: formState.email,
+      password: formState.password,
+    },
+    onCompleted: ({ signup }) => {
+      localStorage.setItem(AUTH_TOKEN, signup.token);
+      navigate("/");
+    },
   });
 
   return (
@@ -54,7 +96,7 @@ const Login = () => {
       <div className="flex mt3">
         <button
           className="pointer mr2 button"
-          onClick={() => console.log("onClick")}
+          onClick={formState.login ? login : signup}
         >
           {formState.login ? "login" : "create account"}
         </button>
